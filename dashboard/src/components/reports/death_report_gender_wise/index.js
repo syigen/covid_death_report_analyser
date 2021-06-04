0efdbaa605562d44865970efb833cfec95b83e32
Dewmal <dewmal@egreen.io>
2021-06-05 02:18:03 +0530

    Create chart component

---
+++ dashboard/src/components/reports/death_report_gender_wise/index.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import PieChartReport from "../../charts/pie_chart";
+
+const DeathReportGenderWise = () => {
+    const [genderCount, setGenderCount] = useState();
+    useEffect(() => {
+        setGenderCount([
+            { value: 1048, name: 'Male' },
+            { value: 735, name: 'Female' },
+        ])
+    }, [])
+
+
+    return (
+        <PieChartReport title={"Gender wise deaths"} data={genderCount} />
+    );
+}
+
+export default DeathReportGenderWise;
