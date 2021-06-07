3c916578bbdbebefeb8387d22e800e4f82bb350a
Dewmal <dewmal@egreen.io>
2021-06-07 15:33:11 +0530

    Update api to productiona nd development

--- dashboard/src/api.js
+++ dashboard/src/api.js
@@ -6,8 +6,11 @@ const Api = async () => {
     const curTime = new Date();
     const should_refresh = false;
     if (!DATA.data || should_refresh) {
-        // const res = await fetch("https://pandemic-info.s3.amazonaws.com/get_json_report.json")
-        const res = await fetch("http://127.0.0.1:7878/get_json_report")
+        let url = "/get_json_report.json"
+        if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+            url = "http://127.0.0.1:7878/get_json_report"
+        }
+        const res = await fetch(url)
         const data = await res.json();
         DATA.data = data
         DATA.created = curTime;
