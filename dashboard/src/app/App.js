037f89579d137564eb3d6e646bb3d8ad524b1450
Dewmal <dewmal@egreen.io>
2021-06-07 15:33:18 +0530

    Add header

--- dashboard/src/app/App.js
+++ dashboard/src/app/App.js
@@ -6,9 +6,26 @@ const App = () => {
 
 
     return (
-        <div className="w-full h-full dark:bg-gray-800">
+        <div className="w-full h-full bg-gray-800">
+
+            <nav class="mb-2 font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-gray-800 shadow sm:items-baseline w-full">
+                <div class="mb-2 sm:mb-0 inner">
+
+                    <a href="/" class="text-2xl text-white no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">SYIGEN-COVID-ANALYZER</a><br />
+                    <span class="text-xs text-gray-50">COVID-19 Pandamic Report Analysis</span>
+
+                </div>
+
+                <div class="sm:mb-0 self-center">
+
+                    <a href="https://github.com/syigen/covid_death_report_analyser" class="text-md no-underline text-white hover:text-blue-100 ml-2 px-1">About us</a>
+
+                </div>
+
+
+            </nav>
             <Dashboard />
-        </div>
+        </div >
     );
 }
 
