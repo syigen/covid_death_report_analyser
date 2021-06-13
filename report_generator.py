7d415acd483bff84b61635bb631942d395958538
Dewmal <dewmal@egreen.io>
2021-06-13 21:05:59 +0530

    announced_dates added to summery report

--- report_generator.py
+++ report_generator.py
@@ -148,11 +148,16 @@ def get_report_date_distribution_summary(df, dates):
 
 def generate_summary_report():
     df = _read_summary_csv()
+    announced_dates = list(df.announced_date.unique())
     dates = list(df.death_record_date.unique())
     dates += list(df.report_date.unique())
     dates += list(df.announced_date.unique())
+
     dates = list(set(dates))
     dates.sort(key=lambda date: datetime.strptime(date, '%Y-%m-%d'))
+
+    announced_dates = list(set(announced_dates))
+    announced_dates.sort(key=lambda date: datetime.strptime(date, '%Y-%m-%d'))
 
     data_set_report_date = {f"{d}": 0 for d in dates}
     data_set_record_date = {f"{d}": 0 for d in dates}
@@ -186,6 +191,7 @@ def generate_summary_report():
 
     data = {
         "dates": list(dates),
+        "announced_dates": list(announced_dates),
         "data": {
             "report_date_count": report_date_count_summary,
             "report_date": report_date_summary,
